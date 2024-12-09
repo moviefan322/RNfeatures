7d295dd3742a769dcce5b8a2659cb236f1c84b07
moviefan322 <philman202@yahoo.com>
2024-12-09 18:05:03 -0500

    configures expo-image-picker

---
+++ components/Places/ImagePicker.tsx
@@ -0,0 +1,20 @@
+import { View, Text, StyleSheet, Image, Button } from "react-native";
+
+const ImagePicker = () => {
+  return (
+    <View>
+      <Text>ImagePicker</Text>
+    </View>
+  );
+};
+
+export default ImagePicker;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
