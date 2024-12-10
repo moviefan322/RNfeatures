c6b36f4f599d835868d5cb5139662771a7b9a913
moviefan322 <philman202@yahoo.com>
2024-12-10 17:44:43 -0500

    adds reactive map marker to map

--- screens/Map.tsx
+++ screens/Map.tsx
@@ -1,15 +1,60 @@
+import { useState } from "react";
 import { StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 
+interface Region {
+  latitude: number;
+  longitude: number;
+  latitudeDelta: number;
+  longitudeDelta: number;
+}
+
+interface Event {
+  nativeEvent: {
+    coordinate: {
+      latitude: number;
+      longitude: number;
+    };
+  };
+}
+
 const Map = () => {
-  const region = {
-    latitude: 40.697594, 
+  const [selectedLocation, setSelectedLocation] = useState({
+    lat: 0,
+    lng: 0,
+  });
+  const region: Region = {
+    latitude: 40.697594,
     longitude: -73.916976,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   };
 
-  return <MapView style={styles.map} initialRegion={region}></MapView>;
+  const selectLocationHandler = (event: Event) => {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+
+    setSelectedLocation({
+      lat,
+      lng,
+    });
+  };
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={region}
+      onPress={selectLocationHandler}
+    >
+      { selectedLocation && <Marker
+        title="Picked Location"
+        coordinate={{
+          latitude: selectedLocation.lat,
+          longitude: selectedLocation.lng,
+        }}
+      />}
+    </MapView>
+  );
 };
 
 export default Map;
